cb078c6cd3e54b3f92b962a40ad20762276ff5c6
Christoffer Klang <christoffer.klang@gmail.com>
2011-03-25 14:47:28 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fbhack::Application.config.secret_token = 'd86722e7b05758008d89ce2f9e02a5f91fea55559621dd5c8fc1feeae033ea88b2cac6c8cc3244d1b17318b0101888d04e8c41465ce996e599297a7a86331217'
